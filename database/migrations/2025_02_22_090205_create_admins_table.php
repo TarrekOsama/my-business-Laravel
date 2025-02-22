45836eed0f2d2ce61f47d57c6f1a01f9cd1312f2
TarrekOsama <tarrekosama@gmail.com>
2025-02-22 12:23:57 +0200

    migrations edited

--- database/migrations/2025_02_22_090205_create_admins_table.php
+++ database/migrations/2025_02_22_090205_create_admins_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('role')->default('admin');
             $table->enum('role', ['admin', 'super_admin'])->default('admin');
             $table->timestamps();
         });
