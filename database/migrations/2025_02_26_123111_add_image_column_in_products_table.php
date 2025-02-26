ac3d8dfe76b921a5f234b133653a15c22c35db7e
TarrekOsama <tarrekosama@gmail.com>
2025-02-26 17:51:45 +0200

    add Update and delete product logic for admin dashboard

--- database/migrations/2025_02_26_123111_add_image_column_in_products_table.php
+++ database/migrations/2025_02_26_123111_add_image_column_in_products_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
     {
         Schema::table('products', function (Blueprint $table) {
             
+            
         });
     }
 };
--- resources/views/user/edit-profile.blade.php
+++ resources/views/user/edit-profile.blade.php
@@ -8,32 +8,44 @@
                     @method('PUT')
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
-                        <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name',$user->name) }}" >
+                        <input type="text" name="name" id="name"
+                        class="form-control @error('name') is-invalid @enderror"
+                        value="{{ old('name',$user->name) }}" >
+                         @error('name')
+                         <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
                     </div>
-                    @error('name')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+                  
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
-                        <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror" value="{{ $user->email }}" >
+                        <input type="email" name="email" id="email" 
+                        class="form-control @error('email') is-invalid @enderror"
+                         value="{{ old('email',$user->email )}}" >
+                         @error('email')
+                         <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
                     </div>
-                    @error('email')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+  
                     <div class="mb-3">
                         <label for="phone_number" class="form-label">Phone Number</label>
-                        <input type="text" name="phone_number" id="phone_number" class="form-control @error('phone_number') is-invalid @enderror" value="{{ $user->phone_number }}" >
-                    </div>
-                    @error('phone_number')
+                        <input type="text" name="phone_number" id="phone_number"
+                         class="form-control @error('phone_number') is-invalid @enderror"
+                          value="{{ old('phone_number',$user->phone_number) }}" >
+                          @error('phone_number')
                         <div class="invalid-feedback">{{ $message }}</div>
                         @enderror
+                    </div>
+                    
                     <div class="mb-3">
                         <label for="address" class="form-label">Address</label>
-                        <input type="text" name="address" id="address" class="form-control @error('address') is-invalid @enderror" value="{{ $user->address }}" >
+                        <input type="text" name="address" id="address"
+                         class="form-control @error('address') is-invalid @enderror" 
+                         value="{{ old('address',$user->address )}}" >
+                         @error('address')
+                         <div class="invalid-feedback">{{ $message }}</div>
+                         @enderror
                     </div>
-                    @error('address')
-                        <div class="invalid-feedback">{{ $message }}</div>
-                        @enderror
+                   
                     <button type="submit" class="btn btn-primary">Update Profile</button>
                     <a href="{{ route('users.show', $user) }}" class="btn btn-secondary">Cancel</a>
                 </form>
