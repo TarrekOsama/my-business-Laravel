54cb4108a4286b9f27956fd68efd9bddd0a3cc05
TarrekOsama <tarrekosama@gmail.com>
2025-02-26 20:25:15 +0200

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -93,7 +93,6 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        logger($request->all());
         
         $request->validate([
             'name' => 'required|string|min:5|max:255|regex:/^[A-Za-z0-9\s]+$/|unique:products,name,' . $product->id,
@@ -101,7 +100,8 @@ class ProductController extends Controller
             'price' => 'required|numeric',
             'stock' => 'required|integer',
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:5120',
-            'categories' => 'required|array'
+            'categories' => 'required|array',
+            'categories.*' => 'exists:categories,id'
         ]);
 
         $productData = $request->except(['image', 'categories']);
@@ -117,8 +117,6 @@ class ProductController extends Controller
             } catch (\Exception $e) {
                 return back()->with('error', 'Image upload failed');
             }
-        } else {
-            $productData['image'] = $product->image;
         }
 
 
@@ -127,7 +125,6 @@ class ProductController extends Controller
             $productData['slug'] = Hash::make($request->slug);
         }
 
-        logger($productData);
 
         $product->update($productData);
 
